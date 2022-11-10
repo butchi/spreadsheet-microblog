b54c2ff9ec8cc7e25f78ce3c33595a42d5993d6f
iwabuchi-yuki-butchi <butchiyu+com-github@gmail.com>
2022-11-10 19:22:52 +0900

    投稿一覧ページ実装

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,63 @@ const { data: sheetData } = await useFetch(sheetUrl)
 
 <template>
     <main>
-        <p>index page</p>
+        <v-row justify="center">
+            <v-col lg="2" md="3">
+                <v-list density="compact">
+                    <v-list-subheader>
+                        Spreadsheet Microblog Demo
+                    </v-list-subheader>
+
+                    <v-list-item v-for="(item, i) in menuItemArr" :key="i" :value="item" active-color="primary">
+                        <template v-slot:prepend>
+                            <v-icon :icon="item.icon"></v-icon>
+                        </template>
+
+                        <v-list-item-title v-text="item.text"></v-list-item-title>
+                    </v-list-item>
+                </v-list>
+
+                <v-btn class="mt-5" color="primary" rounded block>つぶやく</v-btn>
+            </v-col>
+            <v-col md="5">
+                <v-card class="mt-3" v-for="post in postArr.slice().reverse()" :key="post[0]">
+                    <v-list-item class="w-100" three-line>
+                        <template v-slot:prepend>
+                            <v-avatar color="grey-darken-3"
+                                image="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light">
+                            </v-avatar>
+                        </template>
+
+                        <v-list-item-title>山田太郎</v-list-item-title>
+
+                        <v-list-item-subtitle>@yamada_t</v-list-item-subtitle>
+
+                        <v-list-item-subtitle>{{ dateRelative(post[0]) }}</v-list-item-subtitle>
+                    </v-list-item>
+                    <v-card-text>
+                        {{ post[1] }}
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn rounded small disabled>
+                            <v-icon class="mr-1" icon="mdi-comment"></v-icon>
+                            {{ 0 }}
+                        </v-btn>
+                        <v-btn rounded small disabled>
+                            <v-icon class="mr-1" icon="mdi-repeat"></v-icon>
+                            {{ 0 }}
+                        </v-btn>
+                        <v-btn rounded small disabled>
+                            <v-icon class="mr-1" icon="mdi-heart"></v-icon>
+                            {{ 0 }}
+                        </v-btn>
+                        <v-btn rounded small disabled>
+                            <v-icon class="mr-1" icon="mdi-share"></v-icon>
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+            <v-col md="3"></v-col>
+        </v-row>
     </main>
 </template>
 
@@ -17,6 +73,25 @@ import Papa from "papaparse"
 import { DateTime } from "luxon"
 
 export default {
+    data: () => ({
+        menuItemArr: [
+            {
+                text: "ホーム",
+                icon: "mdi-home",
+                value: 1,
+            },
+            {
+                text: "通知",
+                icon: "mdi-bell",
+                value: 2,
+            },
+            {
+                text: "プロフィール",
+                icon: "mdi-account",
+                value: 3,
+            },
+        ],
+    }),
     computed: {
         postArr() {
             return Papa.parse(this.sheetData).data.slice(1)
