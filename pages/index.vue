6f5d6c0e17e94c4217dec8c97b05ca6b080669b5
iwabuchi-yuki-butchi <butchiyu+com-github@gmail.com>
2022-11-10 21:59:30 +0900

    複数人のフィード実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,3 @@
-<script setup>
-const runtimeConfig = useRuntimeConfig()
-
-const { sheetUrl } = runtimeConfig
-
-const { data: sheetData } = await useFetch(sheetUrl)
-</script>
-
 <template>
     <main>
         <v-row justify="center">
@@ -24,25 +16,34 @@ const { data: sheetData } = await useFetch(sheetUrl)
                     </v-list-item>
                 </v-list>
 
-                <v-btn class="mt-5" color="primary" rounded block>つぶやく</v-btn>
+                <v-btn class="mt-5" color="primary" rounded block @click="isOpenDialog = true">つぶやく</v-btn>
+
+                <v-select class="mt-15" v-model="curUserSlug" :items="userLi" item-title="name" item-value="slug">
+                </v-select>
+
+                <v-dialog v-model="isOpenDialog" width="640" height="439">
+                    <iframe :src="`${curUser.formUrl}/viewform?embedded=true`" width="640" height="439" frameborder="0"
+                        marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+                </v-dialog>
             </v-col>
             <v-col md="5">
-                <v-card class="mt-3" v-for="post in postArr.slice().reverse()" :key="post[0]">
+                <v-card class="mt-3" v-for="post in postArr.slice().reverse()" :key="`${post.authorId} ${post.date}`">
                     <v-list-item class="w-100" three-line>
                         <template v-slot:prepend>
-                            <v-avatar color="grey-darken-3"
-                                image="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light">
+                            <v-avatar v-if="post.authorImg" :image="post.authorImg"></v-avatar>
+                            <v-avatar v-else color="grey-darken-3">
+                                <v-icon>mdi-account</v-icon>
                             </v-avatar>
                         </template>
 
-                        <v-list-item-title>山田太郎</v-list-item-title>
+                        <v-list-item-title>{{ post.authorName || defaultName }}</v-list-item-title>
 
-                        <v-list-item-subtitle>@yamada_t</v-list-item-subtitle>
+                        <v-list-item-subtitle>@{{ post.authorSlug || defaultSlug }}</v-list-item-subtitle>
 
-                        <v-list-item-subtitle>{{ dateRelative(post[0]) }}</v-list-item-subtitle>
+                        <v-list-item-subtitle>{{ dateRelative(post.date) }}</v-list-item-subtitle>
                     </v-list-item>
                     <v-card-text>
-                        {{ post[1] }}
+                        {{ post.text }}
                     </v-card-text>
                     <v-card-actions>
                         <v-btn rounded small disabled>
@@ -74,6 +75,10 @@ import { DateTime } from "luxon"
 
 export default {
     data: () => ({
+        isOpenDialog: false,
+        curUserSlug: "anonymous",
+        userArr: [],
+        postArr: [],
         menuItemArr: [
             {
                 text: "ホーム",
@@ -91,15 +96,23 @@ export default {
                 value: 3,
             },
         ],
+        defaultSlug: "anonymous",
+        defaultName: "匿名",
     }),
     computed: {
-        postArr() {
-            return Papa.parse(this.sheetData).data.slice(1)
+        curUser() {
+            return this.userArr.filter(user => user.slug === this.curUserSlug)[0]
+        },
+        userLi() {
+            return this.userArr.map(user => ({
+                slug: user.slug,
+                name: user.name,
+            }))
         },
     },
     methods: {
-        dateRelative(date) {
-            const dateTime = DateTime.fromFormat(date, "yyyy/MM/dd hh:mm:ss")
+        dateRelative(dateStr) {
+            const dateTime = DateTime.fromFormat(dateStr, "yyyy/MM/dd hh:mm:ss").plus({ hours: 9 })
 
             const d = Math.round(- dateTime.diffNow("days").days)
             const h = Math.round(- dateTime.diffNow("hours").hours)
@@ -108,6 +121,27 @@ export default {
 
             return d ? `${d}日前` : h ? `${h}時間前` : m ? `${m}分前` : s ? `${s}秒前` : ""
         }
+    },
+    async mounted() {
+        const res = await fetch(this.$config.sheetUrl)
+
+        const userCsvTxt = await res.text()
+
+        this.userArr = Papa.parse(userCsvTxt).data.slice(1).map(([date, slug, name, sheetUrl, formUrl, img]) => ({ date, slug, name, sheetUrl, formUrl, img }))
+
+        this.userArr.forEach(user => {
+            fetch(user.sheetUrl).then(res => {
+                res.text().then(postCsvTxt => {
+                    const authorSlug = user.slug
+                    const authorName = user.name
+                    const authorImg = user.img
+
+                    const arr = Papa.parse(postCsvTxt).data.slice(1).map(([date, text]) => ({ authorSlug, authorName, authorImg, date, text }))
+
+                    this.postArr = this.postArr.concat(arr)
+                })
+            })
+        })
     },
 }
 </script>
