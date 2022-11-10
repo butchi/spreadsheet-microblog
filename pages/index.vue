4224462b206f0d64de290a7eb30a01779142c296
iwabuchi-yuki-butchi <butchiyu+com-github@gmail.com>
2022-11-10 19:22:17 +0900

    時間算出メソッド実装

--- pages/index.vue
+++ pages/index.vue
@@ -21,5 +21,17 @@ export default {
             return Papa.parse(this.sheetData).data.slice(1)
         },
     },
+    methods: {
+        dateRelative(date) {
+            const dateTime = DateTime.fromFormat(date, "yyyy/MM/dd hh:mm:ss")
+
+            const d = Math.round(- dateTime.diffNow("days").days)
+            const h = Math.round(- dateTime.diffNow("hours").hours)
+            const m = Math.round(- dateTime.diffNow("minutes").minutes)
+            const s = Math.round(- dateTime.diffNow("seconds").seconds)
+
+            return d ? `${d}日前` : h ? `${h}時間前` : m ? `${m}分前` : s ? `${s}秒前` : ""
+        }
+    },
 }
 </script>
