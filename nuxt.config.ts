218770376b7b9980c68884354e16ba98a77e066e
iwabuchi-yuki-butchi <butchiyu+com-github@gmail.com>
2022-11-10 23:04:44 +0900

    meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            title: 'スプレッドシートマイクロブログ デモ',
+            meta: [
+                { name: 'description', content: 'GoogleスプレッドシートでTwitterのようなマイクロブログを実現するデモです。' }
+            ],
+        }
+    },
     css: ["vuetify/lib/styles/main.sass", "mdi/css/materialdesignicons.min.css"],
     build: {
         transpile: ["vuetify"],
